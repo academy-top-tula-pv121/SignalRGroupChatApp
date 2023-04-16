c18e137dc5a2e53751e7ca3140393068f9934836
maxefimov <efimov.top.academy@gmail.com>
2023-04-16 20:23:16 +0300

    Добавьте файлы проекта.

---
+++ SignalRGroupChatApp/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRGroupChatApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task Enter(string userName, string userGroup)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userGroup);
+            await Clients.All.SendAsync("Notify", $"{userName} input chat group of {userGroup}");
+        }
+
+        public async Task Send(string userName, string groupName, string message)
+        {
+            await Clients.Group(groupName).SendAsync("Receive", userName, message);
+        }
+    }
+}
